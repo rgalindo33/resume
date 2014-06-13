64012836d5ce06657eb262f1236b118ab59c9e85
rgalindo33 <rgalindo33@hotmail.com>
2014-06-13 14:26:31 +0200

    pdf generation

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source "https://rubygems.org"
 
 gem 'guard-nanoc'
 gem 'kramdown'
+gem 'pdfkit'
+gem 'pry'
---
+++ commands/generate_pdf.rb
@@ -0,0 +1,18 @@
+usage       'generate_pdf'
+aliases     :pdf
+summary     'makes a pdf version of the latest html'
+description 'takes the actual html site and generates a downloadable pdf'
+
+run do |opts, args, cmd|
+
+  require 'PDFKit'
+  require 'pry'
+
+  html = File.new( "output/index.html" )
+  kit = ::PDFKit.new( html, :page_size => 'letter' )
+  binding.pry
+  kit.stylesheets << 'output/stylesheet.css'
+
+  file = kit.to_file( "output/raul_galindo_resume.pdf" )
+
+end
